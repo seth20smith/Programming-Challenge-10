a6f66c627dbd55ef038ea84d3c9e2aaf38dcd7e9
Smith1 <seth20smith@gmail.com>
2021-12-12 18:31:59 -0800

    Final

--- script.js
+++ script.js
@@ -145,7 +145,7 @@ function writeHtml() {
 
                 cards += `<div class="card-header">
         <h2 class="card-title">${employeeArray[i].getName()}</h2>
-        <h3 class="card-title"><i class="fas mr-2"></i>${employeeArray[i].getRole()}</h3> 
+        <h4 class="card-title"><i class="fas mr-2"></i>${employeeArray[i].getRole()}</h4> 
         
     </div>
     <div class="card-body">
@@ -161,7 +161,7 @@ function writeHtml() {
                 // engineer card
                 cards += `<div class="card-header">
                 <h2 class="card-title">${employeeArray[i].getName()}</h2>
-                <h3 class="card-title"><i class="fas mr-2"></i>${employeeArray[i].getRole()}</h3> 
+                <h4 class="card-title"><i class="fas mr-2"></i>${employeeArray[i].getRole()}</h4> 
                 
             </div>
             <div class="card-body">
@@ -178,7 +178,7 @@ function writeHtml() {
 
                 cards += `<div class="card-header">
         <h2 class="card-title">${employeeArray[i].getName()}</h2>
-        <h3 class="card-title"><i class="fas mr-2"></i>${employeeArray[i].getRole()}</h3> 
+        <h4 class="card-title"><i class="fas mr-2"></i>${employeeArray[i].getRole()}</h4> 
         
     </div>
     <div class="card-body">
@@ -199,10 +199,10 @@ function writeHtml() {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>My Team</title>
+        <title>Thee Employees</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="dist/style.css">
+        <link rel="stylesheet" href="dist/style.css"/>
         <script src="https://kit.fontawesome.com/c502137733.js"></script>
     </head>
 
@@ -210,7 +210,7 @@ function writeHtml() {
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 jumbotron mb-3 team-heading">
-                    <h1 class="text-center">My Team</h1>
+                    <h1 class="text-center">Thee Employees</h1>
                 </div>
             </div>
         </div>
